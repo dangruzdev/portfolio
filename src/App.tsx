d8efc0f95a26f9971c2f9e87e99fbd230b7103eb
Dan <itsdangruzdev@gmail.com>
2023-09-30 11:50:25 +0300

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import "./app.css";
 import avatar from "./assets/peep.svg";
 import jslogo from "./assets/icons8-javascript.svg";
